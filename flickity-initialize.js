df342a8166227d81798edc4c81785fe121eddd46
agustinafeijoo <agustina@theuxdepartment.com>
2018-05-10 15:11:18 -0300

    Fixed images. Smooth scrolling. Writing hovers (bug).

--- flickity-initialize.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var flkty = new Flickity( '.main-carousel',{
-    wrapAround: true
-  });
-  console.log("hola")
-})
-
---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var flkty = new Flickity( '.main-carousel',{
+        wrapAround: true
+    });
+
+    var anchorLinks = document.querySelectorAll('a[href*="#"]')
+
+    for(anchorLink of anchorLinks) {
+        anchorLink.addEventListener('click', function (event) {
+            event.preventDefault()
+            var link = event.currentTarget
+            var id = link.hash
+            var target = document.querySelector(id)
+            
+            target.scrollIntoView({
+                block: 'start',
+                behavior: 'smooth'
+            })
+        })
+    }
+})
+  
